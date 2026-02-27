c12d97f32bd1e9fa65f997278d752ce18c39dd21
“jasson-cardoza” <jassonchiquillo@hotmail.com>
2026-02-26 20:15:30 -0600

    haciendo repo general de la materia ^_____^

---
+++ python/suma.py
@@ -0,0 +1,50 @@
+# es un comentario
+
+## para declarar una variable
+nombre = "jasson"
+print(nombre)
+
+edad = 40
+
+print(nombre, edad, " years old")  ## concatenar
+# binario = 0
+# binario = 1
+# string = letras
+# objeto = 012345
+
+
+# inmutabilidad  :
+# pero siempre deben de conservar su tipo
+
+
+## direccion # 0x
+## tipo => numerico o string ( booleano, objeto)
+## tipo numero => perpara a la memoria
+## para guardar un tipo de dato string exa.
+
+## textos largos vamos a utilizar las comillas triples
+texto_largo = """
+Lorem ipsum dolor sit amet, 
+consectetur adipiscing elit.
+Sed do eiusmod tempor 
+incididunt ut labore et 
+dolore magna aliqua."""
+print(texto_largo)
+## Las comillas triples se usan para textos largos
+# mantiene el formato de lo que se copia entre ellas
+## al momento de compilarlas
+
+numero1 = 10
+numero2 = 20
+
+resultado = numero1 + numero2
+print("El resultado de la suma es:", resultado)
+
+resultado = numero1 - numero2
+print("El resultado de la resta es:", resultado)
+
+resultado = numero1 / numero2
+print("El resultado de la division es:", resultado)
+
+resultado = numero1 * numero2
+print("El resultado de la multiplicacion es:", resultado)
