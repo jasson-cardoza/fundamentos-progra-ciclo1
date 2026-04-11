b97f869bc150417da3e6cb99e2fafa4f610a156f
“jasson-cardoza” <jassonchiquillo@hotmail.com>
2026-04-11 14:17:17 -0600

    Parcial 02 Fundamentos de progración, Ejercicio 3, Ejercicio de remover prefijo

---
+++ Parciales/Parcial_2/Ejercicio3.py
@@ -0,0 +1,9 @@
+# Ejercicio 3
+# 1. Crea una variable con el texto "ING. Su nombre".
+# 2. Remueve el prefijo "ING. " de la cadena.
+# 3. Convierte el texto restante completamente a letras mayusculas.
+
+texto= "ING. Jasson Cardoza";
+print(texto);
+texto= texto.removeprefix("ING. ");
+print(texto);
