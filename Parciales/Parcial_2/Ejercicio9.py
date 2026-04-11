0c30d174739b96f73725fe2427b78e09dbef802e
“jasson-cardoza” <jassonchiquillo@hotmail.com>
2026-04-11 15:02:02 -0600

    Parcial 02 Fundamentos de progración, Ejercicio 9, Ejercicio usando la funcion replace para cambiar any time por always

---
+++ Parciales/Parcial_2/Ejercicio9.py
@@ -0,0 +1,9 @@
+# Ejercicio 9
+# 1. Toma la cadena "Any time y Anytime".
+# 2. Reemplaza todas las apariciones de "Any time" (con espacio) por "Always".
+# 3. Convierte todo el texto resultante a mayusculas.
+
+texto = "Any time y Anytime"
+texto = texto.replace("Any time" , "Always")
+texto = texto.upper()
+print(texto)
