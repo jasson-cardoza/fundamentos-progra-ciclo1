5d6510c9fe7dc8c83ba5623d1cbf1bc720d843c6
“jasson-cardoza” <jassonchiquillo@hotmail.com>
2026-04-18 20:57:25 -0600

    Tarea if y bucles, Fundamentos de programación, Ejercicio #4

---
+++ Tareas/condicionalesYbucles/Ejercicio4.py
@@ -0,0 +1,9 @@
+# Ejercicio 4:
+# Pide un número y determina si es par o impar.
+
+numero = int(input("Ingresa un número: "));
+
+if numero % 2 == 0:
+    print("Es par");
+else:
+    print("Es impar");
