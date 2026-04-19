835c0c88d01cc1d038eba1032ea874fa424d355e
“jasson-cardoza” <jassonchiquillo@hotmail.com>
2026-04-18 20:17:53 -0600

    Tarea if y bucles, Fundamentos de programación, Ejercicio #1

---
+++ Tareas/condicionalesYbucles/Ejercicio1.py
@@ -0,0 +1,11 @@
+# Ejercicio 1
+# Pide un número al usuario e indica si es positivo, negativo o cero usando if, elif y else.
+
+numero = float(input("Ingresa un número: "));
+
+if numero > 0:
+    print("Es positivo")
+elif numero < 0:
+    print("Es negativo")
+else:
+    print("Es cero");
