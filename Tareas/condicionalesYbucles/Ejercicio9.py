e0d567c85ef8043c2cded28e16363416f5fefb39
“jasson-cardoza” <jassonchiquillo@hotmail.com>
2026-04-18 21:09:06 -0600

    Tarea if y bucles, Fundamentos de programación, Ejercicio #9

---
+++ Tareas/condicionalesYbucles/Ejercicio9.py
@@ -0,0 +1,10 @@
+# Ejercicio 9:
+# Solicita un año y determina si es bisiesto.
+
+
+año = int(input("Ingresa un año: "))
+
+if (año % 4 == 0 and año % 100 != 0) or (año % 400 == 0):
+    print("Es bisiesto")
+else:
+    print("No es bisiesto")
